ad3c1f1e502bb91bf89ad1f91f30af296aede340
Nathan Dennler <dennler@usc.edu>
2023-04-29 15:15:32 -0700

    fixing more submodule issues?

---
+++ readme.md
@@ -0,0 +1,5 @@
+First, make sure all the data are synched correctly by running:
+
+```
+stimuli/synch_data.py
+```
---
+++ stimuli/synch_data.py
@@ -0,0 +1,6 @@
+'''
+The purpose of this script is to synchronize the data in all the places it needs to be. 
+This involves copying over all of the sound/audio/kinesthetic files and the csvs that 
+contain the indices for all of the stimuli.
+'''
+
